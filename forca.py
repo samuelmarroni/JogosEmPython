1a40a528fb8118c3570562a61cc790f66d789563
samuel <samuelmarroni@hotmail.com>
2022-04-11 17:55:21 -0300

    Manipulando as strings.

--- forca.py
+++ forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index += 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
